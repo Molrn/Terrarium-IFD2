8bcaf16220790e3234982f2a475e7c76e1fa6f6f
Molrn <76754000+Molrn@users.noreply.github.com>
2021-01-15 10:25:22 +0100

    Update ATMega.h

--- headers/ATMega.h
+++ headers/ATMega.h
@@ -14,13 +14,31 @@
 #define ventil_unset 12
 #define buzzer 11
 
+
+//Affiche une variable sur l'ecran OLED
 void affichagevariable(float (variable),const char* (unite), int (taille));
+
+//Affiche une chaine de characteres sur l'ecran OLED
 void texte(const char* text,int taille);
+
+//Affiche le menu general sur l'ecran OLED
 void affichage_gen();
+
+//affiche le message annoncant comment désactiver le buzzer
 void menu_buzzer();
+
+//actualise les variables en fonction du contenu affiché sur l'ecran OLED lorsqu'il y a appui sur le bouton moins 
 void boutonmoinstest();
+
+//actualise les variables en fonction du contenu affiché sur l'ecran OLED lorsqu'il y a appui sur le bouton plus
 void boutonplustest();
+
+//actualise les variables en fonction du contenu affiché sur l'ecran OLED lorsqu'il y a appui sur le bouton valider
 void boutonvalidertest();
+
+//Active le buzzer 
 void activation_buzzer();
+
+//verifie constamment si les boutons sont appuyés ou non
 void testdesboutons();
 
